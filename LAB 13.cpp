333d003a644c8a5ea6bae28666ee2d4f9533f4b5
KUNAL MADHAVAI <kunal.madhavai24@sanjivani.edu.in>
2024-09-25 01:04:25 +0530

    Add files via upload

---
+++ LAB 13.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream> 
+using namespace std; 
+class Parent { 
+public: 
+int id_p; 
+void printID_p() 
+{ 
+cout << "Base ID: " << id_p << endl; 
+} 
+}; 
+class Child : public Parent { 
+public: 
+int id_c; 
+void printID_c() 
+{ 
+cout << "Child ID: " << id_c << endl; 
+} 
+}; 
+int main() 
+{  
+Child obj1;  
+obj1.id_p = 7; 
+obj1.printID_p(); 
+obj1.id_c = 91; 
+obj1.printID_c(); 
+return 0; 
+} 
