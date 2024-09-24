c8eb2ae73673f433d71cdfa3960314109f71f5a8
KUNAL MADHAVAI <kunal.madhavai24@sanjivani.edu.in>
2024-09-25 00:17:29 +0530

    Add files via upload
    This Program of lab 1 includes of the Binary operator..In the we use to work with two integer value and we use binary AND operator,binary OR operator,binary XOR operator,left shift operator,right shift operator,one's conplement operator...

---
+++ LAB 9.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int a =2, b=4;
+  cout<<"a&b is"<<(a&b)<<endl;
+  cout<<"a|b is"<<(a|b)<<endl;
+  cout<<"a^b is"<<(a^b)<<endl;
+  cout<<"a<<1 is"<<(a<<1)<<endl;
+  cout<<"a>>1 is"<<(a>>b)<<endl;
+  cout<<"-(a) is"<<-(a)<<endl;
+  
+  return 0;
+  
+}
